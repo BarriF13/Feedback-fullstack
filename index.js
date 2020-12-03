ba00abe630194cbd154f3b045c0ba94f6f138444
Barri <info@bdesignstudio.co.uk>
2020-12-03 13:32:14 +0000

    Changed greeting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();//express server
 
 app.get('/', (req ,res)=>{
-  res.send({ hi: 'there'})
+  res.send({ bye: 'buddy'})
 
 });
 // heroku uses this and chose the port if not use 5000
