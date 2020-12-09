3bbc4e9495ebd5bf836295e31f0d161f2df0f5cb
Barri <info@bdesignstudio.co.uk>
2020-12-09 18:31:11 +0000

    Make google auth strategy

---
+++ Readme.md
@@ -0,0 +1,4 @@
+* passport library --one general installation -- one strategy for authenticating with one very specific method like google or facebook or email
+`npm install --save passport passport-google-oauth20`
+----
+deploy with heroku
--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
-
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();//express server
 
-app.get('/', (req ,res)=>{
-  res.send({ bye: 'buddy'})
+const keys = require('./config/keys');
+//Hey passport, I want you to authenticate a user with this strategy and () we say how 
+//clientID and clientSecret
+//https://console.developers.google.com/ -- for registering the app
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  //after grant permission we sent user to url below
+  callbackURL: '/auth/google/callback'
+  }, (accessToken) => {
+    console.log(accessToken);
+  })
+);
 
-});
 // heroku uses this and chose the port if not use 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+//Client ID is ok if it gets shared 
+// Client secret we don't want to share 
