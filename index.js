2578dd4eeb941344fe3baa62a52a7ca0e2167659
Barri <info@bdesignstudio.co.uk>
2020-12-14 12:35:48 +0000

    create mongoose model class

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const keys = require('./config/keys')
 require('./services/passport');//passport.js doesn't return anything so we won't assign in to any variable
 
+require('./models/User'); //Each time we load the app mongoose will make a collection users for us 
 
 const app = express();
 require('./routes/authRoutes')(app);
---
+++ models/User.js
@@ -0,0 +1,20 @@
+//mongoose model class
+
+const mongoose = require('mongoose');
+//Schema is one of the properties in mongoose
+//const Schema = mongoose.Schema; or  de-structure like below
+const { Schema }= mongoose;
+//mongoose like to know all the different properties on each record we want to have,  a head of time. So we use schema
+//with schema object = will describe what each property should look like 
+
+const userSchema = new Schema(
+  {
+    googleId: String
+  }
+);
+
+//To create the model class and tell mongoose to create userSchema :
+//users is the name of collection 
+mongoose.model('users', userSchema);
+
+//now execute the code above we go to index.js 
