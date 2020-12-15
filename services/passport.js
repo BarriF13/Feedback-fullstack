f76eb582e709951b87cbbc30123e7d7bfc46d9dc
Barri <info@bdesignstudio.co.uk>
2020-12-15 11:03:47 +0000

    encoding the user

--- services/passport.js
+++ services/passport.js
@@ -7,6 +7,11 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users'); //User is out model class here 
 
+//for adding cookie-- user here is a database user and id is mongo id 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+})
+
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
@@ -16,20 +21,20 @@ passport.use(new GoogleStrategy({
   (accessToken, refreshToken, profile, done) => {
     //console.log('profile', profile);
     User.findOne({ googleId: profile.id })
-    .then((existingUser)=>{
-      //if we already have a record
-      if(existingUser){
-        //done is for finishing off with mongo first arg is err if there is no error we get null , second arg = user record-everything is done  
-        done(null , existingUser);
-      } else {
-        //we make a new record
-        // we use .save to save it database -mongoose model instance
-        new User({ googleId: profile.id })
-        .save()
-        .then((user)=>{ done(null, user) });
-      }
-    })
-   
+      .then((existingUser) => {
+        //if we already have a record
+        if (existingUser) {
+          //done is for finishing off with mongo first arg is err if there is no error we get null , second arg = user record-everything is done  
+          done(null, existingUser);
+        } else {
+          //we make a new record
+          // we use .save to save it database -mongoose model instance
+          new User({ googleId: profile.id })
+            .save()
+            .then((user) => { done(null, user) });
+        }
+      })
+
   })
 );
 
