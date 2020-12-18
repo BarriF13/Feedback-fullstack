a7e5ffd52de440993230d1904a9c6f895f186931
Barri <info@bdesignstudio.co.uk>
2020-12-18 17:52:54 +0000

    setup the place for action creator to show up in app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,27 +1,31 @@
 //REACT router
 
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
-
 
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-const app = () => {
-  return (
-    <div className="container">
-      <Router>
-       <div>
-         <Header/>
-         <Route exact path="/" component={Landing}/>
-         <Route exact path="/surveys" component={Dashboard}/>
-         <Route  path="/surveys/new" component={SurveyNew}/>
-       </div>
-      </Router>
+class App extends Component {
+  componentDidMount(){
+    
+  }
+  render() {
+    return (
+      <div className="container">
+        <Router>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </Router>
 
-    </div>
-  )
-}
+      </div>
+    )
+  }
+};
 export default app;
