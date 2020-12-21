6b9f71f36a5d6dff352e9cb13fd0bb2397e282b3
Barri <info@bdesignstudio.co.uk>
2020-12-21 18:21:18 +0000

    Added billing and client side app

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ mongoose.connect(keys.mongoURI);
 const app = express();
 //*********** middle wares **********/
 //for parsing data we get form stripe api
-app.use( bodyParser.json());
+app.use(bodyParser.json());
 //now telling express to use cookie--maxAge = how long to expired in milliseconds (days*hours*min*second*millisecond)--Keys for encrypt and we don't put it in public
 app.use(
   cookieSession({
@@ -31,6 +31,21 @@ app.use(passport.session());
 //they return a function 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+
+
+//for express in production mode in heroku
+if (process.env.NODE_ENV === 'production') {
+  //Express will serve up production assets 
+  //like main.js file or main.css files
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
