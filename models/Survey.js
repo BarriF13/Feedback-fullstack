c5cb0ba21bdde3c5d0397149e3cc389b982fbf22
Barri <info@bdesignstudio.co.uk>
2020-12-24 13:05:34 +0000

    create subdocument collection

--- models/Survey.js
+++ models/Survey.js
@@ -1,13 +1,16 @@
 //mongoose model class
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-
+const RecipientSchema = require('./Recipient')
 const surveySchema = new Schema({
   title: String ,
   body: String,
   subject: String,
   //would be emails so it would be an array
-  recipients: [String]
+  recipients: [RecipientSchema],
+  yes: { type: Number, default: 0},
+  no: { type: Number, default: 0},
+
  });
 
  mongoose.model('surveys', surveySchema);
