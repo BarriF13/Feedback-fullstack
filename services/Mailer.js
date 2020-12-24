a3c714404de69f2b5c7886732e30ba48fe0fbe65
Barri <info@bdesignstudio.co.uk>
2020-12-24 17:03:43 +0000

    set up mailer class

--- services/Mailer.js
+++ services/Mailer.js
@@ -5,8 +5,20 @@ const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
 
-//this class get's lot's of configuration and gives out mailer that's why we make it like react class comp
+//this class get lot's of configuration and gives out mailer that's why we make it like react class comp
 class Mailer extends helper.Mail{
-  
+
+  constructor({ subject, recipients}, content){
+    super();
+
+    this.from_email = new helper.Email('no-reply@howdowedo.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients);
+  }
+
+
 }
+
+
 module.exports = Mailer;
