4d6c6700e813b7046b205739e195cf0f8f9d14a7
Barri <info@bdesignstudio.co.uk>
2020-12-25 18:44:38 +0000

    custom field

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,13 @@
+//Survey field contains logic to render a single label and test
+import React from 'react';
+
+
+export default  () => {
+  return (
+    <div>
+      SurveyFiled
+    </div>
+  )
+}
+
+
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,12 +1,24 @@
 // SurveyForm shows a form for the user to add input 
 import React, { Component } from 'react';
-import {reduxForm} from 'redux-form'; //allows to talk to redux store at the top of our app -similar to connect function
+import {reduxForm, Field} from 'redux-form'; //allows to talk to redux store at the top of our app -similar to connect function-- Field is helper for rendering html 
+import SurveyField from './SurveyField';
 
 export class SurveyForm extends Component {
+  renderFields(){
+    return(
+      <div>
+        <Field type="text" name="title" component={SurveyField}/>
+      </div>
+    )
+  }
+
   render() {
     return (
       <div>
-        surveyForm
+        <form onSubmit={ this.props.handleSubmit(values =>console.log(values))}>
+      {this.renderFields()}
+      <button type="submit">Submit</button>
+      </form>
       </div>
     )
   }
