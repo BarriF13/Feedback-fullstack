e0a33b15b22e68dbdb312e531979ba244b5656f0
Barri <info@bdesignstudio.co.uk>
2020-12-25 18:58:29 +0000

    add all the fields

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,13 +1,32 @@
 // SurveyForm shows a form for the user to add input 
 import React, { Component } from 'react';
-import {reduxForm, Field} from 'redux-form'; //allows to talk to redux store at the top of our app -similar to connect function-- Field is helper for rendering html 
+import { reduxForm, Field } from 'redux-form'; //allows to talk to redux store at the top of our app -similar to connect function-- Field is helper for rendering html 
 import SurveyField from './SurveyField';
 
 export class SurveyForm extends Component {
-  renderFields(){
-    return(
+  renderFields() {
+    return (
       <div>
-        <Field type="text" name="title" component={SurveyField}/>
+        <Field
+          label="Survey Title"
+          type="text" name="title"
+          component={SurveyField}
+        />
+           <Field
+          label="Subject Line"
+          type="text" name="subject"
+          component={SurveyField}
+        />
+           <Field
+          label="Email Body"
+          type="text" name="body"
+          component={SurveyField}
+        />
+           <Field
+          label="Recipient List"
+          type="text" name="emails"
+          component={SurveyField}
+        />
       </div>
     )
   }
@@ -15,10 +34,10 @@ export class SurveyForm extends Component {
   render() {
     return (
       <div>
-        <form onSubmit={ this.props.handleSubmit(values =>console.log(values))}>
-      {this.renderFields()}
-      <button type="submit">Submit</button>
-      </form>
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          {this.renderFields()}
+          <button type="submit">Submit</button>
+        </form>
       </div>
     )
   }
